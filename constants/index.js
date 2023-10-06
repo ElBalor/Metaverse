98ec847b3819474ff66981e466c9c763c755e8c3
ElBalor <heylelyaka@gmail.com>
2023-10-06 10:52:24 +0100

    Initial commit

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'The Hogwarts',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'The Upside Down',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Kadirojo Permai',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Paradise Island',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Hawkins Labs',
+  },
+];
+
+export const startingFeatures = [
+  'Find a world that suits you and you want to enter',
+  'Enter the world by reading basmalah to be safe',
+  'No need to beat around the bush, just stay on the gas and have fun',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+        'we have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: ' EL BALOR',
+    subtitle:
+        'Invest time in becoming digitally literate.Learn about the technologies that power the meterverse such as virtual reality, augmented reality, blockchain and NFTs(Non-fungible Tokens)',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: 'Xavier',
+    subtitle:
+        'There are various metaverse platforms emerging each with its own feature, building relationships and engaging with communities in the metaverse can be as important as in the physical world.Join discussions attend Virtual events and network',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'MIA',
+    subtitle:
+        'Dont just consume in the metaverse, consider how you can contribute and create in the virtual world,Whether through art content or experiences. Be cautious about sharing personal informations and be aware of potential fraud and scams in the virtual spaces.',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
